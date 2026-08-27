b6cddb4674b4c06dcd1e5df34c74995d1816a788
William-Patrick Barr <145700970+Willpatbarr@users.noreply.github.com>
2026-08-27 02:32:55 -0600

    more setup

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, Laminar Flow Backend!")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	addr := ":" + port
+	log.Printf("laminarflow backend listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
